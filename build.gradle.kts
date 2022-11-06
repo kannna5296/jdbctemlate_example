97da38c11746879b887e9423c1d9e7b29025682e
kannna5296 <kannnahacks5296@gmail.com>
2022-11-06 18:23:29 +0900

    Ver指定

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc:2.7.3")
 	implementation("org.springframework.boot:spring-boot-starter-web:2.7.3")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+	runtimeOnly("com.h2database:h2:2.1.214")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.3")
 }
 
 tasks.withType<KotlinCompile> {
